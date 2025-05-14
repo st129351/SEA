e2f9c65d40d2acbda3c4bafd4094d3a046b642c8
st129351 <st129351@student.spbu.ru>
2025-05-14 21:25:14 +0300

    add new features in the graphics

--- analisys/normal.py
+++ analisys/normal.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+from scipy.integrate import simpson
 import numpy as np
 
 df = pd.read_csv('/Users/daniillickovaha/Documents/learning/SQL/projects/SEA/bd/dataset.csv')
@@ -42,29 +43,45 @@ def normal_distr(mid, disp, name, color):
     sigm = np.sqrt(disp)
     # creating 100 points in range mid - 3*sigm, mid + 3*sigm
     # in this range lies 99.7% of data
-    x = np.linspace(mid - 4 * sigm, mid + 4 * sigm, 100) # more points - smoother the line
+    x = np.linspace(mid - 4 * sigm, mid + 4 * sigm, 100) # more points give precision
     plt.plot(x, norm.pdf(x, mid, sigm), color = color, label = name) # x=[1, 2, 3], y=[0.1, 0.4, 0.5], color='red', name='a'
-    mx_y = norm.pdf(mid, mid, sigm)
+    mx_y = norm.pdf(mid, mid, sigm) # probability density function
     # norm.pdf() 1st arg - point, array of points for calculating
     # 2nd - math expect, 3d - standart deviation
+
     # scatter - for draw point
-    plt.fill_between(x, norm.pdf(x, mid, sigm), color = color, alpha = 0.2)
-    plt.scatter(mid, mx_y, color = 'black', marker = 'o', label = f'mean for {name} = {round(mid)}', zorder = 10) 
+    plt.scatter(mid, mx_y, color = 'black', marker = 'o', label = f'mean for {name} = {round(mid)}, \n(sigma = {round(sigm)})', zorder = 10) 
     # zorder - position on the z-axis
-    plt.plot([mid, mid], [0, mx_y], '--', color = color, alpha = 0.8) # vertical dotted line 
+    # plt.plot([mid, mid], [0, mx_y], '--', color = color, alpha = 0.6) # vertical dotted line 
     # ([start_x, end_x], [start_y, end_y])
 
     # for interval (mean - sigm, mean + sigm)
-    plt.plot([mid - sigm, mid - sigm], [0, norm.pdf(mid - sigm, mid, sigm)], '--', color = color, alpha = 0.6)
-    plt.plot([mid + sigm, mid + sigm], [0, norm.pdf(mid + sigm, mid, sigm)], '--', color = color, alpha = 0.6)
+    plt.plot([mid - sigm, mid - sigm], [0, norm.pdf(mid - sigm, mid, sigm)], '--', color = 'black', alpha = 0.4)
+    plt.plot([mid + sigm, mid + sigm], [0, norm.pdf(mid + sigm, mid, sigm)], '--', color = 'black', alpha = 0.4)
     # for interval (mean - 2sigm, mean + 2sigm)
-    plt.plot([mid - 2*sigm, mid - 2*sigm], [0, norm.pdf(mid - 2*sigm, mid, sigm)], '--', color = color, alpha = 0.45)
-    plt.plot([mid + 2*sigm, mid + 2*sigm], [0, norm.pdf(mid + 2*sigm, mid, sigm)], '--', color = color, alpha = 0.45)
+    plt.plot([mid - 2*sigm, mid - 2*sigm], [0, norm.pdf(mid - 2*sigm, mid, sigm)], '--', color = 'black', alpha = 0.35)
+    plt.plot([mid + 2*sigm, mid + 2*sigm], [0, norm.pdf(mid + 2*sigm, mid, sigm)], '--', color = 'black', alpha = 0.35)
     # for interval (mean - 3sigm, mean + 3sigm)
-    plt.plot([mid - 3*sigm, mid - 3*sigm], [0, norm.pdf(mid - 3*sigm, mid, sigm)], '--', color = color, alpha = 0.3)
-    plt.plot([mid + 3*sigm, mid + 3*sigm], [0, norm.pdf(mid + 3*sigm, mid, sigm)], '--', color = color, alpha = 0.3)
+    plt.plot([mid - 3*sigm, mid - 3*sigm], [0, norm.pdf(mid - 3*sigm, mid, sigm)], '--', color = 'black', alpha = 0.3)
+    plt.plot([mid + 3*sigm, mid + 3*sigm], [0, norm.pdf(mid + 3*sigm, mid, sigm)], '--', color = 'black', alpha = 0.3)
 
+    # the area under the prob density graph = 1
 
+    x1 = np.linspace(mid - sigm, mid + sigm, 100)
+    y1 = norm.pdf(x1, mid, sigm)
+    area1 = round(simpson(y=y1, x=x1), 2)
+    plt.fill_between(x1, y1, color = color, alpha = 0.2, label = f'square = {area1}')
+
+    x2 = np.linspace(mid - 2*sigm, mid + 2*sigm, 100)
+    y2 = norm.pdf(x2, mid, sigm)
+    area2 = round(round(simpson(y=y2, x=x2) - area1, 2), 2)
+    plt.fill_between(x2, y2, color = color, alpha = 0.15, label = f'square = {area2}')
+
+    x3 = np.linspace(mid - 3*sigm, mid + 3*sigm, 100)
+    y3 = norm.pdf(x3, mid, sigm)
+    area3 = round(round(simpson(y=y3, x=x3)) - area2 - area1, 2)
+    plt.fill_between(x3, y3, color = color, alpha = 0.1, label = f'square = {area3}')
+    
 def call_figure(mid, disp, name, color):
     # creating the new graphic window
     normal_distr(mid, disp, name, color)
@@ -73,11 +90,14 @@ def call_figure(mid, disp, name, color):
     plt.title('distribution of the number of participants by event type')
     plt.xlabel('number of participants')
     plt.ylabel('probability density')
+
     plt.legend() # post all of the labels in the right places
+
     plt.grid(True) # turn on grid
-    plt.show()
+    # set text on graph, bbox - white window for text
+    # plt.show()
+    plt.savefig(f'/Users/daniillickovaha/Documents/learning/SQL/projects/SEA/outputs/{name}.png', dpi = 324)
 
 call_figure(mid_c, disp_c, 'Conference', 'red')
 call_figure(mid_o, disp_o, 'Olympiad', 'blue')
 call_figure(mid_s, disp_s, 'Seminar', 'green')
-
